45766c7c21d4dc6e053959aadb9fa0d7433bfb7b
David Luecke <daff@neyeon.com>
2012-06-21 10:05:48 -0600

    Updating build and table_scroll

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 steal('funcunit')
 .then(
-	'./accordion/accordion_test.js',
-	'./table_scroll/table_scroll_test.js'
-	// './block/block_test.js'
+	'../accordion/accordion_test.js'
+	// './table_scroll/table_scroll_test.js',
+	// './block/block_test.js',
 	// './fills/fills_test.js',
 	// './grid/grid_test.js',
 	// './modal/modal_test.js',
