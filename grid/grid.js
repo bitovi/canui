2da4c80a426e9f067cabc7c8fbb3b7b971341305
David Luecke <daff@neyeon.com>
2012-06-27 11:27:21 -0600

    Grid examples and improvements

--- grid/grid.js
+++ grid/grid.js
@@ -1,36 +1,45 @@
-steal('can/control', 'can/view/ejs', 'can/observe').then(function() {
+steal('can/control', 'can/view/ejs', 'can/observe').then(
+'//canui/grid/views/init.ejs', '//canui/grid/views/row.ejs',
+'//canui/grid/views/single.ejs', '//canui/grid/views/head.ejs',
+function() {
+	var computes = [ 'columns', 'list', 'emptyText' ];
+
 	can.Control('can.ui.Grid', {
 		defaults : {
 			select : {
 				heading : 'thead',
 				body : 'tbody',
 				header : 'th',
-				row : 'tr',
+				row : 'tbody tr.grid-row',
 				column : 'td'
 			},
 			view : {
 				init : '//canui/grid/views/init.ejs',
 				row : '//canui/grid/views/row.ejs',
-				empty : '//canui/grid/views/empty.ejs',
+				single : '//canui/grid/views/single.ejs',
 				head : '//canui/grid/views/head.ejs'
 			},
-			emptyText : 'No data'
+			emptyText : 'No data',
+			loadingText : 'Loading...'
 		}
 	}, {
 		setup : function(el, ops) {
-			// Convert the options for list and columns into a can.compute
-			var options = can.extend({}, ops, {
-				list : can.compute(ops.list || {}),
-				columns : can.compute(ops.columns || [])
+			var options = can.extend({}, ops);
+			// Convert computed properties
+			can.each(computes, function(name) {
+				options[name] = can.compute(ops[name] || {});
 			});
 			can.Control.prototype.setup.call(this, el, options);
 		},
 
 		init : function() {
 			this.element.html(this.render('init'));
-			this.heading = this.find('heading');
+			this.heading = this.find('heading').html(this.render('head', {
+				options : this.options,
+				columns : this.options.columns()
+			}));
 			this.body = this.find('body');
-			this.heading.html(this.render('head', { options : this.options, columns : this.options.columns() }));
+			this.element.trigger('init', this);
 			this.draw();
 		},
 
@@ -40,9 +49,9 @@ steal('can/control', 'can/view/ejs', 'can/observe').then(function() {
 		 * @return {can.ui.Grid} The Grid control instance
 		 */
 		draw : function() {
-			var body = this.body.empty(),
-				self = this,
-				data = this.options.list();
+			var data = this.options.list();
+
+			this.body.empty();
 
 			// Only do this for actual observable lists
 			if(data instanceof can.Observe.List) {
@@ -50,47 +59,50 @@ steal('can/control', 'can/view/ejs', 'can/observe').then(function() {
 				this.on();
 			}
 
-			data.each(function(item) {
-				body.append(self.render('row', { options : self.options, columns : self._row(item), item : item }));
-			});
+			if(!data.length) {
+				this.singleColumn(this.options.emptyText);
+			} else {
+				can.each(data, can.proxy(this.appendRow, this));
+			}
+
+			this.element.trigger('redraw', this);
 
 			return this;
 		},
 
-		/**
-		 * Converts a data item into a list of can.computes according to the columns.
-		 *
-		 * @param item
-		 * @return {Array}
-		 * @private
-		 */
-		_row : function(item) {
-			var res = [];
-			can.each(this.options.columns(), function(column) {
+		appendRow : function(item, index) {
+			var columnData = [],
+				self = this;
+
+			can.each(self.options.columns(), function(column) {
+				var defaultCallback = function() {
+					return can.isFunction(item.attr) ? item.attr(column.attr) : item[column.attr];
+				};
+
 				var callback = can.isFunction(column.attr) ?
-						can.proxy(column.attr, item) :
-						function() {
-							return item.attr(column.attr);
-						};
-				res.push(can.compute(callback));
+						function() { return column.attr(item, index); } :
+						defaultCallback;
+
+				columnData.push(can.compute(callback));
 			});
-			return res;
+
+			self.body.append(self.render('row', {
+				options : self.options,
+				columns : columnData,
+				item : item
+			}));
 		},
 
-		// When the columns change we can reinitialize everything
-		'{columns} change' : 'init',
-		'{list} change' : 'draw',
-
-		// TODO these should just remove and add their row
-		'{data} remove' : 'draw',
-		'{data} add' : 'draw',
-
-		columns : function(cols) {
-			return this.options.columns(cols);
+		singleColumn : function(text) {
+			this.body.append(this.render('single', {
+				text : text,
+				options : this.options
+			}));
 		},
 
-		list : function(data) {
-			return this.options.list(data);
+		' loading' : function(el, ev, text) {
+			this.body.empty();
+			this.singleColumn(text || this.options.loadingText);
 		},
 
 		find : function(selector) {
@@ -99,6 +111,22 @@ steal('can/control', 'can/view/ejs', 'can/observe').then(function() {
 
 		render : function(name, options) {
 			return can.view(this.options.view[name] || name, options || this);
+		},
+
+		// When the columns change we can reinitialize everything
+		'{columns} change' : 'init',
+		// Redraw if someone updates the list
+		'{list} change' : 'draw',
+
+		// We will redraw everything as removing and adding items might trigger other things as well
+		'{data} remove' : 'draw',
+		'{data} add' : 'draw'
+	});
+
+	// Create direct accessors for computed properties
+	can.each(computes, function(name) {
+		can.ui.Grid.prototype[name] = function(data) {
+			return this.options[name](data);
 		}
 	});
 });
