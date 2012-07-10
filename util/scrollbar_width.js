e64852c03128b2e4f46affdf66a818c9fc8251b5
David Luecke <daff@neyeon.com>
2012-07-10 15:50:05 -0600

    can.ui.scrollbarWidth()

--- util/scrollbar_width.js
+++ util/scrollbar_width.js
@@ -1,27 +1,32 @@
 steal('jquery').then(function ($) {
 	window.can || (window.can = {});
 	window.can.ui || (window.can.ui = {});
-	window.can.ui.scrollbarWidth = function() {
-		var div = $('<div id="out"><div style="height:200px;"></div></div>').css({
-				position : "absolute",
-				top : "0px",
-				left : "0px",
-				visibility : "hidden",
-				width : "100px",
-				height : "100px",
-				overflow : "hidden"
-			}).appendTo(document.body),
-			inner = $(div[0].childNodes[0]),
-			w1 = inner[0].offsetWidth,
-			w2;
 
-		div.css("overflow", "scroll");
-		var w2 = inner[0].offsetWidth;
-		if (w2 == w1) {
-			inner.css("width", "100%"); //have to set this here for chrome
-			w2 = inner[0].offsetWidth;
+	var cached = null;
+	window.can.ui.scrollbarWidth = function() {
+		if(width === null) {
+			var cached = $('<div id="out"><div style="height:200px;"></div></div>').css({
+					position : "absolute",
+					top : "0px",
+					left : "0px",
+					visibility : "hidden",
+					width : "100px",
+					height : "100px",
+					overflow : "hidden"
+				}).appendTo(document.body),
+				inner = $(div[0].childNodes[0]),
+				w1 = inner[0].offsetWidth,
+				w2;
+
+			div.css("overflow", "scroll");
+			var w2 = inner[0].offsetWidth;
+			if (w2 == w1) {
+				inner.css("width", "100%"); //have to set this here for chrome
+				w2 = inner[0].offsetWidth;
+			}
+			div.remove();
+			cached = w1- w2;
 		}
-		div.remove();
 
 		/**
 		 * @parent canui
@@ -33,6 +38,6 @@ steal('jquery').then(function ($) {
 		 *      $('#element').width($('#element').width()
 		 *          - can.ui.scrollbarWidth);
 		 */
-		return w1 - w2;
+		return cached;
 	}
 });
