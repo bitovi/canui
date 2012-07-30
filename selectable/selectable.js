d00d715bd7845e0ad43975bf817b23004ae1f6dd
David Luecke <daff@neyeon.com>
2012-07-30 08:23:38 -0600

    Selectable fix

--- selectable/selectable.js
+++ selectable/selectable.js
@@ -102,7 +102,7 @@ $.event.special.activate = {
  *       outsideDeactivate: false
  *     })
  */
-Control('can.ui.Selectable',{
+can.Control('can.ui.Selectable',{
 	pluginName : 'selectable',
     defaults : {
         // what can be selected
