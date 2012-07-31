6dbb072f18601d756b6163c2635a92249db4ca96
David Luecke <daff@neyeon.com>
2012-07-30 23:30:01 -0600

    First updated Grid draft

--- grid/grid.js
+++ grid/grid.js
@@ -1,44 +1,52 @@
-steal('can/control', 'can/view/ejs', 'can/observe', 'can/control/plugin').then(
-'//canui/grid/views/init.ejs', '//canui/grid/views/row.ejs',
-'//canui/grid/views/single.ejs', '//canui/grid/views/head.ejs',
-function() {
-	// A list of properties that should be turned into can.computes and be directly accessible
-
-
+steal('jquery', 'canui/list', 'can/view/ejs', function($) {
 	var proto = can.ui.List.prototype;
 	can.ui.List('can.ui.Grid', {
 		pluginName : 'grid',
 		defaults : {
-			header : '',
-			view : '//canui/grid/views/row.ejs',
-			emptyContent : '',
-			loadingContent : '',
+			header : '//canui/grid/views/head.ejs',
+			view : function(observe) {
+				// TODO this isn't nice yet
+				var row = [], self = this;
+				can.each(this.options.columns, function(col) {
+					row.push(can.isFunction(col.content) ? col.content.call(self, observe, col) :
+						can.compute(function() {
+							return observe.attr(col.content);
+						}));
+				});
+				return can.view('//canui/grid/views/row.ejs', { row : row, cid : observe._cid });
+			},
+			emptyContent : function() {
+				return '<tr><td colspan="'
+					+ this.options.columns.length
+					+ '" class="empty">No data</td></tr>'
+			},
+			loadingContent : function() {
+				return '<tr><td colspan="'
+					+ this.options.columns.length
+					+ '" class="loading">Loading...</td></tr>'
+			},
 			scrollable : false
 		}
 	}, {
+		setup : function(el, ops) {
+			proto.setup.apply(this, arguments);
+			this.$ = {
+				table : this.element
+			};
+			this.element = can.$('<tbody>').appendTo(this.$.table);
+			this.$.body = this.element;
+			return [this.element, ops];
+		},
+
 		init : function(el, ops) {
-			this.element.append(can.view(this.options.header, this.options.columns));
+			this.$.table.prepend(can.view(this.options.header, this.options.columns));
+			this.$.header = this.$.table.find('thead');
 			proto.init.apply(this, arguments);
+		},
+
+		destroy : function() {
+			delete this.$;
+			proto.destroy.apply(this, arguments);
 		}
 	});
-
-//	var computes = [ 'columns', 'list', 'emptyText', 'loadingText' ];
-//	// Create direct accessors for computed properties
-//	can.each(computes, function(name) {
-//		if(!can.ui.Grid.prototype[name]) {
-//			can.ui.Grid.prototype[name] = function(data) {
-//				// Computes need to be directly assigned
-//				if(data && data.isComputed) {
-//					this.options[name] = data;
-//					// Rebind listeners (for {name} change)
-//					this.on();
-//					// Call the compute and update the data
-//					this._update(data());
-//					return data;
-//				}
-//
-//				return this.options[name](data);
-//			}
-//		}
-//	});
 });
