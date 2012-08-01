f49200595be270b0b45fed62ad5cc9e3905f60df
Brian Moschel <brian@jupiterjs.com>
2012-08-01 13:27:35 -0400

    "Updating release"

--- release/latest/canui.js
+++ release/latest/canui.js
@@ -541,8 +541,10 @@ module['canui/table_scroll/table_scroll.js'] = (function ($) {
 				$td.css({
 					"padding-top" : 0,
 					"padding-bottom" : 0,
+					"border-top" : 'none',
+					"border-bottom" : 'none',
 					margin : 0,
-					width : ""
+					width : ''
 				}) // If padding is removed from the cell sides, layout might break!
 				$spacer.outerWidth(width + 2).css({
 					"float" : "none",
@@ -1807,6 +1809,183 @@ module['canui/split/split.js'] = (function($) {
 		}
 	})
 })(module["jquery"], module["can/control/control.js"], module["can/control/plugin/plugin.js"], module["jquery/event/drag/limit/limit.js"], module["jquery/dom/dimensions/dimensions.js"], module["jquery/event/key/key.js"], module["jquery/event/resize/resize.js"]);
+module['canui/list/list.js'] = (function($) {
+	can.Control('can.ui.List', {
+		pluginName : 'list',
+		defaults : {
+			attribute : 'data-cid',
+			cid : '_cid'
+		}
+	}, {
+		init : function() {
+			this._cidMap = {};
+			this.update();
+		},
+
+		/**
+		 * Updates the options and re-renders the list.
+		 *
+		 * @param {Object} [options] The updated options
+		 */
+		update : function(options) {
+			can.Control.prototype.update.call(this, options);
+			var list = this.options.list;
+			if(can.isFunction(list)) {
+				list = can.compute(list);
+			}
+			if(list && list.isComputed) {
+				// TODO doesn't trigger
+				this.on(list, 'change', can.proxy(function(ev, newVal) {
+					this._update(newVal);
+				}, this));
+				list = list();
+			}
+			this._update(list);
+		},
+
+		/**
+		 * Updates the data list and sets this.options.data. Converts Arrays
+		 * and waits for Deferreds.
+		 *
+		 * @param {can.Deferred|can.Observe.List|Array} data The data to set
+		 * @private
+		 */
+		_update : function(data) {
+			data = data || [];
+			if(can.isDeferred(data)) {
+				this.element.html(this._fnOption('loadingContent'));
+				data.done(can.proxy(this._update, this));
+			} else {
+				var cidMap = {};
+				this.options.data = data instanceof can.Observe.List ? data : new can.Observe.List(data);
+				// Update the mapping from can.Observe unique id to Observe instance
+				this.options.data.forEach(function(observe) {
+					cidMap[observe._cid] = observe;
+				});
+				this._cidMap = cidMap;
+				this.on();
+				this._render(this._rows(this.options.data));
+			}
+		},
+
+		/**
+		 * Returns a can.$ wrapped list of rendered rows for the given observes.
+		 *
+		 * @param {Array|can.Observe.List} observes The observables to render
+		 * @return {can.$} A can.$ wrapped list of rendered rows
+		 * @private
+		 */
+		_rows : function(observes) {
+			observes = can.makeArray(observes);
+			var rows = $.map(observes, can.proxy(function(observe) {
+				if(can.isFunction(this.options.view)) {
+					return this.options.view.call(this, observe);
+				}
+				return can.view(this.options.view, observe);
+			}, this));
+			return can.$(rows);
+		},
+
+		/**
+		 * Renders the row element list. If the rows are empty or there
+		 * are no rows, the content set in the `empty` option will be rendered.
+		 *
+		 * @param rows
+		 * @private
+		 */
+		_render : function(rows) {
+			var content = !rows || rows.length === 0 ? this._fnOption('emptyContent') : rows;
+			this.element.html(content);
+			this.element.trigger('rendered');
+		},
+
+		_fnOption : function(name, args) {
+			var val = this.options[name];
+			return can.isFunction(val) ? val.apply(this, args || []) : val;
+		},
+
+		'{data} length' : function(list, ev, length) {
+			if(length === 0) {
+				this._render();
+			}
+		},
+
+		'{data} remove' : function(list, ev, observes) {
+			if(list.length) { // We can't get rowElements from an empty list
+				this.rowElements(observes).remove();
+				this.element.trigger('changed');
+			}
+		},
+
+		'{data} add' : function(list, ev, observes) {
+			var rowElements = this.rowElements(),
+				newRows = this._rows(observes);
+			if(rowElements.length) {
+				// We either append after the last data row
+				rowElements.last().after(newRows);
+			} else {
+				// Or prepend it to the element
+				this.element.prepend(newRows);
+			}
+			this.element.trigger('changed');
+		},
+
+		/**
+		 * Returns all rows for a list of observables.
+		 *
+		 * @param arg
+		 * @return {*}
+		 */
+		rowElements : function(arg) {
+			if(!arg) {
+				return this.element.find('[' + this.options.attribute + ']');
+			}
+
+			var elements = [],
+				observes = can.isArray(arg) ? arg : can.makeArray(arguments);
+
+			can.each(observes, can.proxy(function(current) {
+				var row = this.element.find('[' + this.options.attribute + '="' + current[this.options.cid] + '"]')[0];
+				elements.push(row || null);
+			}, this));
+
+			return can.$(elements);
+		},
+
+		/**
+		 * Returns all
+		 *
+		 * @param {Collection} rows An array or DOM element collection
+		 * @return {can.Observe.List|can.Observe}
+		 */
+		list : function(rows) {
+			if(!rows) {
+				return this.options.data || new can.Observe.List();
+			}
+
+			var result = new can.Observe.List(),
+				map = this._cidMap;
+
+			can.each(can.makeArray(rows), function(row) {
+				row = row[0] || row;
+				// Either use getAttribute or the name itself as the index
+				// that way you can pass a list of ids as well
+				var id = row.getAttribute('data-cid');
+				if(map[id]) {
+					result.push(map[id]);
+				}
+			});
+
+			return result;
+		},
+
+		destroy : function() {
+			// Remove all DOM element references
+			delete this.el;
+			can.Control.prototype.destroy.apply(this, arguments);
+		}
+	});
+})(module["jquery"], module["can/control/control.js"], module["can/control/plugin/plugin.js"], module["can/view/view.js"], module["can/observe/observe.js"]);
 
 window.define = module._define;
 })();
